4ebe711950d85d854556caf18136d94113c61680
caturodiaz <caturodiaz@users.noreply.github.com>
2021-05-10 16:00:57 -0300

    Agregar proyecto a GitHub

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import styled from "@emotion/styled";
+import Quote from "./components/Quote";
+import imagen from "./logo.webp";
 
-function App() {
+
+const Button = styled.button`
+background: -webkit-linear-gradient(top left, #a85400 0%, #a85400 40%, #804000 100%);
+background-size: 300px;
+font-family: Arial, Helvetica, sans-serif;
+color: #ffffff;
+margin-top: 3 rem;
+padding: 1rem 3rem;
+font-size: 2rem;
+border: 2px solid black;
+transition: background-size .8s ease;
+:hover{
+  cursor:pointer;
+  background-size: 400px;
+
+}
+`;
+
+const Imagen = styled.img`
+  align: center-top;
+  max-width: 350px;
+
+`
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+function App() {  
+
+  //state de frase
+  const [phrase, getQuote] = useState({});
+  
+  const callAPI = async () => {
+    const api = await fetch("https://got-quotes.herokuapp.com/quotes")
+    const phrase = await api.json()
+    getQuote(phrase)
+  }
+
+  //Cargar una frase
+  useEffect( ()=>{
+    callAPI();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+        <Imagen 
+          src={imagen}
+          alt="logo"
+        />
+        <Quote 
+          phrase={phrase}
+        />
+        <Button
+          onClick={callAPI}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Get Quote
+        </Button>
+    </Container>
   );
 }
 
---
+++ src/components/Quote.jsx
@@ -0,0 +1,59 @@
+import React  from 'react';
+import styled from "@emotion/styled";
+import PropTypes from "prop-types";
+
+const QuoteContainer = styled.div`
+    margin: 2rem;
+    padding: 3rem;
+    border-radius: .5rem;
+    background-color: #ffffff;
+    max-width: 600px;
+    @media (min-width: 992px){
+        margin-top: 10rem;
+    }
+
+    h1{
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        position: relative;
+        padding-left: 4rem;
+
+        &::before {
+            content: open-quote;
+            font-size: 10rem;
+            color: black;
+            position: absolute;
+            left: -1rem;
+            top: -2rem;
+        }
+    }
+
+    p{ 
+        font-family: Geneva, Tahoma, sans-serif;
+        font-size: 1.6rem;
+        font-weight: bold;
+        text-align: right;
+        color: #666;
+        margin-top: 2rem;
+    }
+
+
+`
+
+const Quote = ({phrase}) => {
+    return (
+        
+        <QuoteContainer>
+            <h1>{phrase.quote}</h1>
+            <p>{phrase.character}</p>
+        </QuoteContainer>
+        
+     );
+}
+ 
+
+Quote.propTypes ={
+    titulo: PropTypes.string.isRequired
+}
+export default Quote;
+
